a72b3eab3ab371e1d7eb972d344151faf759c0fb
Imogen-D <imogen.dumville@gmail.com>
2020-11-12 15:06:59 +0100

    Updating Code 12.11.2020

--- allOTUcode.R
+++ allOTUcode.R
@@ -12,54 +12,20 @@ library(taxize)
 library(stringr)
 
 set_entrez_key("ee1b29805250345f705302e643b1bfc4e007")
-full_otu <- read.delim("~/MEME/Uppsala_Katja_Project/Metagenomics/reindeer_kraken2_otu_table_merged_201106.txt", row.names=1, stringsAsFactors=FALSE)
-metadata <- read.csv("~/MEME/Uppsala_Katja_Project/Metagenomics/Sample_processing_masterlist.csv", stringsAsFactors=FALSE)
-colnames(full_otu) <- str_remove(colnames(full_otu), "_kraken2_report")
+full_otu <- read.delim("~/MEME/Uppsala_Katja_Project/Metagenomics/reindeer_kraken2_otu_table_merged_201112-otu.fungi.txt", row.names=1, stringsAsFactors=FALSE)
+metadata <- read.delim("~/MEME/Uppsala_Katja_Project/Metagenomics/reindeer_sample_metadate_merged.txt", stringsAsFactors=FALSE)
+nodupmeta <- metadata[-c(which(duplicated(metadata$Seq.label))),]
+is.na(nodupmeta$Seq.label) #14
+nodupmeta <- nodupmeta[-14,]
+rownames(nodupmeta) <- nodupmeta$Seq.label
 
-reindeermeta <- metadata[which(str_detect(metadata$Seq.label, "Rt")),]
-
-reindeermeta[38,5] <- "Rt052" #previously Vl001/Rt052
 
 #maybe tidy up metadata?? 
-
-classes <- data.frame()
-
-classes <- classification(rownames(full_otu), db="ncbi")
-
-Fungi <- which(str_detect(classes, pattern = "Fungi"))
-
-
-
-for (x in rownames(full_otu)) {
-  class <- classification(x, db = "ncbi")
-  classes <- rbind.data.frame(classes, class)
-}
-
-###old code from other script
-
-OTUtaxa <- rownames(full_otu)
-
-all_kingdoms <- data.frame()
-
-
-for (x in OTUtaxa) {
-  y <- tax_name(x, "kingdom", db = "ncbi")
-  Sys.sleep(0.1)  #ugly and slow, could be shorter but required as API only allows certain no of search per seconds
-  all_kingdoms <- rbind.data.frame(all_kingdoms, y)
-}
-
-funginums <- which(all_kingdoms$kingdom == "Fungi")
-fungisamples <- all_kingdoms[funginums,]
-
-fungaltaxa <- DC1.fungal.otu[,fungisamples$query]
-
-#currently have OTU table of 5 rt samples and table of OTU counts of fungi
-#now: analyse
-
-otu_table <- otu_table(fungaltaxa, taxa_are_rows = FALSE)
-sampledata <- sample_data(samplereindeer)
-phydata <- phyloseq(otu_table, sampledata)
-plot_bar(phydata, fill="Sample.weight.g")
+OTU <- otu_table(full_otu, taxa_are_rows = FALSE)
+sampledata <- sample_data(nodupmeta)
+phylo
+phydata <- phyloseq(OTU, sampledata)
+plot_bar(phydata, fill="Sample.no")
 
 #next = make OTU on x axis & colour by sample/metadata. Go through metadata for interests
-
+colnames(nodupmeta)
