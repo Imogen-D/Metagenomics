60a7bc18dbbc3fa21810dc233730080249965211
Imogen-D <imogen.dumville@gmail.com>
2020-11-16 16:19:09 +0100

    entries with all 0's are removed, using do.call instead of bind_rows

--- scripts/allOTUcode.R
+++ scripts/allOTUcode.R
@@ -12,62 +12,59 @@ library(taxize)
 library(stringr)
 library(tidyr)
 library(data.table)
+library(ggplot2)
 
 setwd("~/MEME/Uppsala_Katja_Project/Metagenomics") #for local script
 
 set_entrez_key("ee1b29805250345f705302e643b1bfc4e007")
-full_otu <- read.delim("./data/reindeer_kraken2_otu_table_merged_201112-otu.fungi.txt", row.names=1, stringsAsFactors=FALSE)
-metadata <- read.delim("./data/reindeer_sample_metadate_merged.txt", stringsAsFactors=FALSE)
-#nodupmeta <- metadata[-c(which(duplicated(metadata$Seq.label))),]
-metadata <- metadata[-c(which(is.na(metadata$Seq.label))),] #removing unsequenced data
-#is.na(nodupmeta$Seq.label) #14
 
-#nodupmeta <- nodupmeta[-14,]
-
-
-rownames(metadata) <- metadata$Seq.label
+full_otu <- read.delim("./data/reindeer_kraken2_otu_table_merged_201112-otu.fungi.txt", row.names=1, stringsAsFactors=FALSE) %>% 
+  replace(., is.na(.), 0) %>% 
+  select(which(colSums(.) > 0)) # remove taxa summing to zero
 
 colnames(full_otu) <- str_replace(colnames(full_otu), "X", "")
 
+metadata <- read.delim("./data/reindeer_sample_metadate_merged.txt", stringsAsFactors=FALSE) %>% 
+  distinct(Seq.label,.keep_all=T) %>%  # remove duplicates, the tidyr way
+  filter(!is.na(Seq.label))
+rownames(metadata)<-metadata$Seq.label # add rownames
 
 #maybe tidy up metadata?? 
 OTU <- otu_table(full_otu, taxa_are_rows = FALSE)
 
 #making TaxonomyTable
-#OTUtaxa <- classification(colnames(full_otu), db = "ncbi")
-#bound1 <- bind_rows(OTUtaxa, .id = "column_label")
-#write.csv(bound1, "./data/OTUtaxonomy.csv")
+OTUtaxa <- classification(colnames(full_otu), db = "ncbi")
+bound1<-do.call(dplyr::bind_rows,OTUtaxa)
+write.csv(bound1, "./data/OTUtaxonomy.csv")
 
-#OTUtaxonomy <- read.csv("~/MEME/Uppsala_Katja_Project/Metagenomics/data/OTUtaxonomy.csv", row.names=1, stringsAsFactors=FALSE)
-#OTUtaxonomy <- OTUtaxonomy[,c(1:3)] #remove id column, otherwise messes with wide function
-#wide <- pivot_wider(OTUtaxonomy, names_from = rank, values_from = name, id_cols = column_label)
-#wide <- wide[,-c(2,4)]
-#wide <- as.data.frame(wide, row.names = column_label)
-#wide <- mutate_all(wide, as.character)
-#rownames(wide) <- wide$column_label
-#wide <- wide[,-1]
-#write.csv(wide, "./data/OTUtaxonomyformatted.csv")
+wide <- bound1 %>% distinct(name,rank,id,.keep_all = T) %>% 
+  pivot_wider(id_cols = id, names_from = rank, values_from = name) %>% 
+  mutate_all(as.character) %>% 
+  mutate(genus.species=paste0(genus," ",species))
+rownames(wide)<-wide$id # add rownames
+  
+write.csv(wide, "./data/OTUtaxonomyformatted.csv")
 
-OTUtaxonomyformatted <- read.csv("~/MEME/Uppsala_Katja_Project/Metagenomics/data/OTUtaxonomyformatted.csv", row.names=1, stringsAsFactors=FALSE)
+OTUtaxonomyformatted <- read.csv("./data/OTUtaxonomyformatted.csv", row.names=1, stringsAsFactors=FALSE)
 OTUtaxonomyformatted <- as.matrix(OTUtaxonomyformatted) #matrix required for tax table
 
 taxotable <- tax_table(OTUtaxonomyformatted)
-sampledata <- sample_data(metadata)
+sampledata <- sample_data(metadata[sample_names(OTU),]) # only take the samples that are present in the OTU table
 
-phydata <- phyloseq(OTU, sampledata, taxotable)
+phydata <- phyloseq(OTU, sampledata,taxotable)
 
 #subsetting for quicker analysis
 noeco <- (which(is.na(metadata$Reindeer.ecotype)))
 ecotypemeta <- sample_data(metadata[-c(noeco),]) #only those with ecotype data
-ecophy <- phyloseq(OTU, ecotypemeta, taxotable)
+ecophy <- phyloseq(OTU, ecotypemeta,taxotable)
 
 topN <- 100
 most_abundant_taxa <- sort(taxa_sums(ecophy), TRUE)[1:topN]
 GP100 <- prune_taxa(names(most_abundant_taxa), ecophy)
 
-plot_bar(GP100, "Reindeer.ecotype", fill="family") # facet_grid=~Reindeer.ecotype
+plot_bar(GP100,x = "Seq.label", fill="species")+theme(legend.position="none") #+facet_grid=~Reindeer.ecotype
 
 #heatmap function not working yet
-plot_heatmap(GP100, sample.label = "Reindeer.ecotype", taxa.label = "family") #SampleType could be metadata - bind 1 + 2 and other data
+plot_heatmap(GP100, sample.label = "Seq.label", taxa.label = "species") #SampleType could be metadata - bind 1 + 2 and other data
 
 
--- scripts/reindeer_merged_kraken2.R
+++ scripts/reindeer_merged_kraken2.R
@@ -1,7 +1,7 @@
 library(tidyverse)
-meta <- read.csv("Metagenomics/reindeer_sample_metadate_merged.txt",sep="\t",header=T)
+meta <- read.csv("Metagenomics/data/reindeer_sample_metadate_merged.txt",sep="\t",header=T)
 
-euk<-read.table("Metagenomics/kraken2_otu_table_merged_201112.txt",sep="\t",header=T,skip = 1,comment.char = "") %>% 
+euk<-read.table("DC-Fungi/kraken2_otu_table_merged_bracken_201113.txt",sep="\t",header=T,skip = 1,comment.char = "") %>% 
   rename(taxa="X.OTU.ID") %>%
   mutate(taxa=paste0("X",taxa)) %>% 
   pivot_longer(cols=-taxa) %>% 
@@ -10,9 +10,9 @@ euk<-read.table("Metagenomics/kraken2_otu_table_merged_201112.txt",sep="\t",head
   column_to_rownames("name")
 
 source("DC2/scripts/grab-taxa.R")
-grab_taxa(euk,"DC-Fungi/reindeer_kraken2_otu_table_merged_201112-taxa.csv")
+grab_taxa(euk,"DC-Fungi/kraken2_otu_table_merged_bracken_201113-taxa.csv")
 
-taxa_names<-read.csv("DC-Fungi/reindeer_kraken2_otu_table_merged_201112-taxa.csv",header=T,na.strings = c("NA","")) %>% filter(kingdom=="Fungi")
+taxa_names<-read.csv("DC-Fungi/kraken2_otu_table_merged_bracken_201113-taxa.csv",header=T,na.strings = c("NA","")) %>% filter(kingdom=="Fungi")
 
 taxa_names$genus.species<-ifelse(is.na(taxa_names$species),
                                  ifelse(is.na(taxa_names$genus),
@@ -29,4 +29,4 @@ rt.euk<-euk %>%
 
 # colnames(rt.euk) <- ifelse(taxa_names$tax_id %in% colnames(rt.euk),taxa_names$species,taxa_names$tax_id)
 
-write.table(rt.euk,file = "Metagenomics/reindeer_kraken2_otu_table_merged_201112-otu.fungi.txt",quote = F,row.names = T,col.names = T,sep="\t")
+write.table(rt.euk,file = "Metagenomics/data/reindeer_kraken2_otu_table_merged_201112-otu.fungi.txt",quote = F,row.names = T,col.names = T,sep="\t")
