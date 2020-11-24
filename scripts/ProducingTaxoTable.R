832cb2d1188526f8fe303a4f844d48a04c6d7dfd
Imogen-D <imogen.dumville@gmail.com>
2020-11-24 11:15:59 +0100

    A plot that actually works + annotated scripts

---
+++ scripts/ProducingTaxoTable.R
@@ -0,0 +1,12 @@
+#Script to produce taxonomy table (in combination with allOTUcode.R)
+
+set_entrez_key("ee1b29805250345f705302e643b1bfc4e007")
+
+#making TaxonomyTable
+OTUtaxa <- classification(colnames(full_otu), db = "ncbi")
+
+bound1<-bind_rows(as_tibble(cbind(OTUtaxa))) %>%
+  select(kingdom,phylum,class,order,family,genus,species)
+rownames(bound1)<-names(OTUtaxa)
+
+write.csv(bound1, "./data/OTUtaxonomyformatted.csv")
--- scripts/allOTUcode.R
+++ scripts/allOTUcode.R
@@ -1,6 +1,4 @@
-#Analysing OTU and metagenomic data for all reindeer samples; extracting fungal taxa
-#phyloseq
-#12112020 OTU without fungi so new OTU table
+#script to produce heatmap for top 20 fungal families in reindeer samples
 
 library(usethis)
 use_git_config(user.name = "Imogen-D", user.email = "imogen.dumville@gmail.com")
@@ -16,74 +14,37 @@ library(microbiomeutilities)
 
 setwd("~/MEME/Uppsala_Katja_Project/Metagenomics") #for local script
 
-#set_entrez_key("ee1b29805250345f705302e643b1bfc4e007")
+#making OTU table
 full_otu <- read.delim("./data/reindeer_kraken2_otu_table_merged_201112-otu.fungi.txt",na.strings = c("","NA"), row.names=1, stringsAsFactors=FALSE) %>% 
   replace(., is.na(.), 0) %>% 
   select(which(colSums(.) > 0)) # remove taxa summing to zero
-
 colnames(full_otu) <- str_replace(colnames(full_otu), "X", "")
+OTU <- otu_table(full_otu, taxa_are_rows = FALSE)
 
+#reading metadata table
 metadata <- read.delim("./data/reindeer_sample_metadate_merged.txt", stringsAsFactors=FALSE) %>% 
   distinct(Seq.label,.keep_all=T) %>%  # remove duplicates, the tidyr way
   filter(!is.na(Seq.label))
 rownames(metadata)<-metadata$Seq.label # add rownames
 
-OTU <- otu_table(full_otu, taxa_are_rows = FALSE)
-
-#making TaxonomyTable
-OTUtaxa <- classification(colnames(full_otu), db = "ncbi")
-
-bound1<-bind_rows(as_tibble(cbind(OTUtaxa))) %>%
-  select(kingdom,phylum,class,order,family,genus,species)
-rownames(bound1)<-names(OTUtaxa)
-
-write.csv(bound1, "./data/OTUtaxonomyformatted.csv")
-
+#reading and formatting taxonomy table
 OTUtaxonomyformatted <- read.csv("./data/OTUtaxonomyformatted.csv", row.names=1, stringsAsFactors=FALSE) %>% # read in taxa table saved from taxize 
   rename_all(str_to_title)  # make the column names into title case
-
 taxotable <- tax_table(as.matrix(OTUtaxonomyformatted)) # matrix required for tax table
 sampledata <- sample_data(metadata[sample_names(OTU),]) # only take the samples that are present in the OTU table
 
+#maing full phyloseq data format
 phydata <- phyloseq(OTU, sampledata,taxotable)
 
-#subsetting for quicker analysis
+#subsetting for quicker analysis removing those without ecotype information
 noeco <- (which(is.na(metadata$Reindeer.ecotype)))
-ecotypemeta <- sample_data(metadata[-c(noeco),]) #only those with ecotype data
+ecotypemeta <- sample_data(metadata[-c(noeco),])
 ecophy <- phyloseq(OTU, ecotypemeta,taxotable)
 
-topN <- 100
-most_abundant_taxa <- sort(taxa_sums(ecophy), TRUE)[1:topN]
-GP100 <- prune_taxa(names(most_abundant_taxa), ecophy)
-
-plot_heatmap(GP100,sample.label = "Seq.label",taxa.label = "Family", max.label = ntaxa(GP100)) + facet_grid(~ Reindeer.ecotype,scales="free",space="free_x")
-
-
-#3000 OTU
-top3000 <- 3000
-most_abundant_taxa_3000 <- sort(taxa_sums(ecophy), TRUE)[1:top3000]
-GP3000 <- prune_taxa(names(most_abundant_taxa_3000), ecophy)
-plot_heatmap(GP3000, sample.label = "Seq.label", taxa.label = "Family", max.label = ntaxa(GP3000)) + facet_grid(~ Reindeer.ecotype, scales="free",space="free_x")
-
-#pdf(file = "./images/100eco.pdf", height = 50, width = 50)
-#plot_heatmap(GP100, sample.label = "Reindeer.ecotype", taxa.label = "species") #SampleType could be metadata - bind 1 + 2 and other data
-#dev.off()
-
-pdf(file = "./images/heatmap100wide.pdf", height = 25, width = 50)
-
-plot_taxa_heatmap(ecophy,
-                  subset.top = 100, taxonomic.level = "Family", 
-                  transformation = "log10", VariableA = "Reindeer.ecotype")
-dev.off()
-
+#aggregating at family level
 OTUfam <- microbiome::aggregate_taxa(ecophy, "Family")
 
-pdf(file = "./images/heatmap1000family.pdf", height = 25, width = 50)
-plot_taxa_heatmap(OTUfam, subset.top = 1000, transformation = "log10",
-                  taxonomic.level = "Family",
-                  VariableA = "Reindeer.ecotype")
-dev.off()
-
+#creating heatmap with clr transformation, top 20 families
 pdf(file = "./images/heatmap20familyclr.pdf", height = 5, width = 10)
 plot_taxa_heatmap(OTUfam, subset.top = 20, transformation = "clr",
                   taxonomic.level = "Family", border_color = "grey60",
