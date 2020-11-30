5b34d4f9427daac5888177d9f979ff43b7a9936f
Imogen-D <imogen.dumville@gmail.com>
2020-11-30 21:04:37 +0100

    First draft contaminant script

--- scripts/WithContaminants.R
+++ scripts/WithContaminants.R
@@ -1,4 +1,4 @@
-#script to produce heatmap for top 20 fungal families in reindeer samples
+#script to produce heatmap for top 20 fungal families in reindeer samples WITH CONTAMINANTS REMOVED
 
 library(usethis)
 use_git_config(user.name = "Imogen-D", user.email = "imogen.dumville@gmail.com")
@@ -11,18 +11,24 @@ library(tidyr)
 library(data.table)
 library(ggplot2)
 library(microbiomeutilities)
+library(decontam)
+
 
 setwd("~/MEME/Uppsala_Katja_Project/Metagenomics") #for local script
 
+full_otu <- read.delim("./data/reindeer_kraken2_otu_table_merged_201129-otu.fungi.txt",na.strings = c("","NA"), row.names=1, stringsAsFactors=FALSE)
+
 #making OTU table
-full_otu <- read.delim("./data/reindeer_kraken2_otu_table_merged_201112-otu.fungi.txt",na.strings = c("","NA"), row.names=1, stringsAsFactors=FALSE) %>% 
-  replace(., is.na(.), 0) %>% 
-  select(which(colSums(.) > 0)) # remove taxa summing to zero
+#couldn't do this filtering (and didn't seem to need to) as removed heaps of taxa
+#full_otu <- read.delim("./data/reindeer_kraken2_otu_table_merged_201129-otu.fungi.txt",na.strings = c("","NA"), row.names=1, stringsAsFactors=FALSE) %>% 
+ # replace(., is.na(.), 0) %>% 
+  #select(which(colSums(.) > 0)) # remove taxa summing to zero
 colnames(full_otu) <- str_replace(colnames(full_otu), "X", "")
 OTU <- otu_table(full_otu, taxa_are_rows = FALSE)
 
+
 #reading metadata table
-metadata <- read.delim("./data/reindeer_sample_metadate_merged.txt", stringsAsFactors=FALSE) %>% 
+metadata <- read.delim("./data/Sample_processing_masterlist.txt", stringsAsFactors=FALSE) %>% 
   distinct(Seq.label,.keep_all=T) %>%  # remove duplicates, the tidyr way
   filter(!is.na(Seq.label))
 rownames(metadata)<-metadata$Seq.label # add rownames
@@ -38,16 +44,23 @@ bound1<-bind_rows(as_tibble(cbind(OTUtaxa))) %>%
   select(kingdom,phylum,class,order,family,genus,species)
 rownames(bound1)<-names(OTUtaxa)
 
-write.csv(bound1, "./data/OTUtaxonomyformatted.csv")
+write.csv(bound1, "./data/OTUtaxonomyformattedwcont.csv")
 
 #reading and formatting taxonomy table
-OTUtaxonomyformatted <- read.csv("./data/OTUtaxonomyformatted.csv", row.names=1, stringsAsFactors=FALSE) %>% # read in taxa table saved from taxize 
+OTUtaxonomyformatted <- read.csv("./data/OTUtaxonomyformattedwcont.csv", row.names=1, stringsAsFactors=FALSE) %>% # read in taxa table saved from taxize 
   rename_all(str_to_title)  # make the column names into title case
 taxotable <- tax_table(as.matrix(OTUtaxonomyformatted)) # matrix required for tax table
 sampledata <- sample_data(metadata[sample_names(OTU),]) # only take the samples that are present in the OTU table
 
 #maing full phyloseq data format
 phydata <- phyloseq(OTU, sampledata,taxotable)
+contaminants <- isContaminant(phydata, neg = TRUE)
+
+View(contaminants)
+length(contaminants)
+which(contaminants$contaminant == TRUE)
+
+nocontan <- isNotContaminant(phydata)
 
 #subsetting for quicker analysis removing those without ecotype information
 noeco <- (which(is.na(metadata$Reindeer.ecotype)))
