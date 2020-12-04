d0fd17c77753458213ef516a3e6c091d8a892d13
Imogen-D <imogen.dumville@gmail.com>
2020-12-04 18:03:20 +0100

    init with renv

--- init.R
+++ init.R
@@ -1,8 +1,10 @@
 ###### R ENVIRONMENT INIT #####
 # First time, initiate the conda environment
-# conda env create --file environment.yaml
+# conda create -n microbiomes-env -c conda-forge r-base=4.0.2 r-devtools r-biocmanager r-renv 
 # all other times after this, conda activate metagenomics-env
 
-# missing packages in yaml
-BiocManager::install(pkgs = c("phyloseq","Biostrings","microbiome"))
-devtools::install_github("microsud/microbiomeutilities")
+# library(renv)
+# renv::init()
+# renv::snapshot()
+
+renv::restore()
