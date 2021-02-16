9bd06ed1766cdb84e229b1c75fe6447becb2f0f1
AdrianForsythe <forsytae@mcmaster.ca>
2021-02-16 15:48:48 +0100

    cleaning up directory, various fixes to Refiltering.R. this may solve issues with read/otu name mismatches. continuing working on desktop version

--- scripts/Refiltering.R
+++ scripts/Refiltering.R
@@ -3,33 +3,27 @@
 ## related to (rownames(top_samples) %in% rownames(top_blanks)
 ##Visulisations/tidy up of code
 
-library(dplyr)
-library(tidyverse)
-library(phyloseq)
 library(usethis)
 use_git_config(user.name = "Imogen-D", user.email = "imogen.dumville@gmail.com")
-library(reshape2)
-library(dplyr)
+
+library(tidyverse)
 library(phyloseq)
-library(tidyr)
-library(ggplot2)
+library(reshape2)
 library(microbiomeutilities)
 library(decontam)
 source("scripts/ancom_v2.1.R")
 library(vegan)
-library(devtools)
 library(nlme)
-library(tidyverse)
 library(compositions)
 library(pairwiseAdonis)
 
-
-##Making object for ABUNDANCES -  BRACKEN output
+##### ABUNDANCES -  BRACKEN output #####
+## phyloseq object with abundance data
 full_otu <- read.delim("./data/kraken2_otu_table_merged_210203-otu.fungi.txt",na.strings = c("","NA"), stringsAsFactors=FALSE) %>% 
   select(which(colSums(.) > 0)) %>%   # remove empty taxa
   filter(rowSums(.) > 0) # remove empty samples
 
-colnames(full_otu) <- str_replace(colnames(full_otu), "X", "")
+## OTU table
 OTU <- otu_table(full_otu, taxa_are_rows = FALSE)
 
 #reading metadata table
@@ -42,35 +36,61 @@ metadata <- read.delim("./data/Sample_processing_masterlist.txt", stringsAsFacto
                                         ifelse(Sample.R_cat == "" & is.na(Sample.R_cat),"unknown",Reindeer.ecotype))))
 rownames(metadata)<-metadata$Seq.label # add rownames
 
-#reading and formatting taxonomy table, made with classification
-OTUtaxonomyformatted <- read.csv("./data/OTUtaxonomyformattedwcont.csv", row.names=1, stringsAsFactors=FALSE) %>% # read in taxa table saved from taxize 
-  rename_all(str_to_title)  # make the column names into title case
-taxotable <- tax_table(as.matrix(OTUtaxonomyformatted))
+# only take the samples that are present in the OTU table
+sampledata <- sample_data(metadata[sample_names(OTU),])
 
-sampledata <- sample_data(metadata[sample_names(OTU),]) # only take the samples that are present in the OTU table
+# reading and formatting taxonomy table, made with classification
+OTUtaxonomyformatted <- read.csv("../taxa.csv", stringsAsFactors=FALSE) %>% # read in taxa table saved from taxize
+  rename_all(str_to_title) %>%    # make the column names into title case
+  mutate(Tax_id = paste0("X",Tax_id))  # adding the prefix prevents some downstream issues
+  
+taxotable <- OTUtaxonomyformatted %>% 
+  filter(Tax_id %in% taxa_names(OTU)) %>% 
+  as.data.frame(.) %>% 
+  tax_table(.) # only include the taxa that are found in the OTU table
+
+# only use this to make row names if taxa match the OTU table
+rownames(taxotable)<-OTUtaxonomyformatted$Tax_id[OTUtaxonomyformatted$Tax_id %in% taxa_names(OTU)]
 
 # making full phyloseq data format
-phydata <- phyloseq(OTU, sampledata, taxotable)
-
-##Making phyloseq object for read information
-reads <- read.delim("./data/kraken2_otu_table_merged_210203-reads.fungi.txt",na.strings = c("","NA"), stringsAsFactors=FALSE) %>% 
-  select(which(colSums(.) > 0)) %>%   # remove empty taxa
-  filter(rowSums(.) > 0) # remove empty samples
-
-colnames(reads) <- str_replace(colnames(reads), "X", "")
-readcounts <- otu_table(reads, taxa_are_rows = FALSE)
-readphydata <- phyloseq(readcounts, sampledata, taxotable) #need to redo taxonomy table
+phydata <- phyloseq(OTU, sampledata,taxotable)
 
 #Only extraction # not with reindeer = BE103 and bear swabs BS003, BS005 - remove
 #sample_names(phydata) != 
 wanted <- !(sample_names(phydata) %in% c("BE103", "BS003", "BS005"))
 phydata <- prune_samples(wanted, phydata)
-wanted <- !(sample_names(readphydata) %in% c("BE103", "BS003", "BS005"))
-readphydata <- prune_samples(wanted, readphydata)
+
+# saving a "base" object, for later use
+saveRDS(phydata,file = "data/phloseq-otu-base.rds")
+
+##Making phyloseq object for read information
+reads <- read.table("~/Downloads/kraken2_otu_table_merged_210203-reads.txt",na.strings = c("","NA"), stringsAsFactors=FALSE) %>% 
+  select(which(colSums(.) > 0))
+
+readcounts <- otu_table(reads, taxa_are_rows = FALSE)
+
+# remake taxa table with read taxa
+read_taxotable <- OTUtaxonomyformatted %>% 
+  filter(Tax_id %in% taxa_names(OTU)) %>% 
+  as.data.frame(.) %>% 
+  tax_table(.) # only include the taxa that are found in the OTU table
+
+# only use this to make row names if taxa match the OTU table
+rownames(read_taxotable)<-OTUtaxonomyformatted$Tax_id[OTUtaxonomyformatted$Tax_id %in% taxa_names(OTU)]
+
+# slight mismatch in samples between otu and reads
+read_sampledata <- sample_data(metadata[sample_names(readcounts),])
+
+# making full phyloseq data format
+readphydata <- phyloseq(readcounts, read_sampledata)
+
+#Only extraction # not with reindeer = BE103 and bear swabs BS003, BS005 - remove
+readwanted <- !(sample_names(readphydata) %in% c("BE103", "BS003", "BS005"))
+readphydata <- prune_samples(readwanted, readphydata)
+
 
 #NOT PRUNING
 #ABUNDANCE Fitlering
-
 # abundance filtering function
 abundance_filter_f <- function(count.mat, cutoff) {
   #count.mat must be in format of samples = rows and taxa = columns
@@ -88,54 +108,71 @@ abundance_filter_f <- function(count.mat, cutoff) {
   return(count.filt)
 }
 
+## run abundance filtering of taxa on otu data
 phy.filt <- abundance_filter_f(as.data.frame(otu_table(phydata)), 0.0005) %>%
   select(which(colSums(.) > 0)) %>%   # remove empty taxa
   filter(rowSums(.) > 0)
-phydata.filt <- phyloseq(otu_table(phy.filt, taxa_are_rows = FALSE), sampledata, taxotable)
+phydata.filt <- phyloseq(otu_table(phy.filt, taxa_are_rows = FALSE), sampledata)
 
+# what taxa were excluded?
+filteredout <- which(!taxa_names(phydata) %in% taxa_names(phydata.filt))
+length(filteredout)
+
+## run abundance filtering of taxa on read data
 phyread.filt <- abundance_filter_f(as.data.frame(otu_table(readphydata)), 0.0005) %>%
   select(which(colSums(.) > 0)) %>%   # remove empty taxa
   filter(rowSums(.) > 0)
-phydata.filt.read <- phyloseq(otu_table(read.filt, taxa_are_rows = FALSE), sampledata, taxotable)
+phydata.filt.read <- phyloseq(otu_table(phyread.filt, taxa_are_rows = FALSE), sampledata)
 
-filteredout <- which(!sample_names(phydata) %in% sample_names(phydata.filt))
-s <- sample_names(phydata)
-s[filteredout] #"Rt022" "Rt066" "BE207" "BE210" "BkL2"  "BL105" "BL106" "BL107" "BL110" "BL112"
-
-read.filteredout <- which(!sample_names(readphydata) %in% sample_names(phydata.filt.read))
-sr <- sample_names(readphydata)
-sr[read.filteredout] #"BE210" "BkL2"  "BL105" "BL106" "BL107" "BL110" "BL112" "Rt022" "Rt066"
+# what taxa were excluded?
+read.filteredout <- which(!taxa_names(readphydata) %in% taxa_names(phydata.filt.read))
+length(read.filteredout)
 
 ##### CONTAMINANT FILTERING #####
+### decontam on OTU
 # create single variable for control samples
 controls<-phydata.filt@sam_data$Sample.R_cat %in% c("ExtBlank","LibBlank","Swab")
+
+# filter contaminants that are in blanks or are significantly associated with the amount of DNA input for sequencing
 both.contaminants <- isContaminant(phydata.filt, method="either",neg = controls,conc = "Seq.copies.in.pool",threshold = c(0.1,0.5))
+
+# how many contaminants were filtered out?
 table(both.contaminants$contaminant)
+
+# keep just the non-contaminant taxa
 phywocont <- prune_taxa(both.contaminants$contaminant==FALSE,phydata.filt)
+
+# keep just the contaminant taxa
 phyWcont <- prune_taxa(both.contaminants$contaminant==TRUE,phydata.filt)
 
-
-#decontam on reads data
+### decontam on reads data
+# create single variable for control samples
 readcontrols<-phydata.filt.read@sam_data$Sample.R_cat %in% c("ExtBlank","LibBlank","Swab")
+
+# filter contaminants that are in blanks or are significantly associated with the amount of DNA input for sequencing
 both.contaminants.read <- isContaminant(phydata.filt.read, method="either",neg = readcontrols,conc = "Seq.copies.in.pool",threshold = c(0.1,0.5))
+
+# how many contaminants were filtered out?
 table(both.contaminants.read$contaminant)
 
+# keep just the non-contaminant taxa
 decontaminantsreadphy <- prune_taxa(both.contaminants.read$contaminant==FALSE, phydata.filt.read)
+
+# keep just the contaminant taxa
 readphyWcont <- prune_taxa(both.contaminants.read$contaminant==TRUE,phydata.filt.read)
 
+#####
+## Decontam OTU
+## Just keep reindeer
+rt.samples <- sample_names(phywocont)[which(grepl("^Rt",sample_names(phywocont)))]
+phywocont.rt <- prune_samples(rt.samples,phywocont)
 
-
-
-
-meta_data <- data.frame(sample_data(phywocont)) %>% rownames_to_column("SampleID")
-rt.samples <- meta_data %>% filter(grepl("^Rt",SampleID))
-phywocont.rt <- prune_samples(rt.samples$SampleID,phywocont)
 ##remove taxa without any reads - not required so none are blank only
 ##which(colSums(otu.rt) == 0) ##0
 
 ##just extraction blanks - whats not there?
-blanksamples <- meta_data %>% filter(grepl("Blank",Sample.R_cat))
-phywocont.blanks <- prune_samples(blanksamples$SampleID,phywocont)
+blanksamples <- sample_names(phywocont)[which(grepl("^B",sample_names(phywocont)))]
+phywocont.blanks <- prune_samples(blanksamples,phywocont)
 
 otu.blanks <- data.frame(otu_table(phywocont.blanks)) %>%
   select(which(colSums(.) > 0))  # remove empty taxa
@@ -144,8 +181,8 @@ blank <- colnames(otu.blanks)
 phyblanks <- prune_taxa(blank, phywocont.blanks)
 
 ##just swabs
-swabsamples <- meta_data %>% filter(grepl("Swab",Sample.R_cat))
-phywocont.swabs <- prune_samples(swabsamples$SampleID, phywocont)
+swabsamples <- sample_names(phywocont)[which(grepl("^BS",sample_names(phywocont)))]
+phywocont.swabs <- prune_samples(swabsamples, phywocont)
 otu.swabs <- data.frame(otu_table(phywocont.swabs)) %>%
   select(which(colSums(.) > 0))  # remove empty taxa
 colnames(otu.swabs) <- str_replace(colnames(otu.swabs), "X", "")
@@ -153,14 +190,12 @@ swab <- colnames(otu.swabs)
 physwabs <- prune_taxa(swab, phywocont.swabs)
 
 ##how many overlap between swabs and blanks?
-overlapphy <- prune_taxa(taxa_names(physwabs), phyblanks) #383 taxa
-
+overlapphy <- intersect(taxa_names(physwabs), taxa_names(phyblanks)) #168 taxa
 
 ##so now need to do all of above but with read data
 ##only reindeer
-meta_data_read <- data.frame(sample_data(decontaminantsreadphy)) %>% rownames_to_column("SampleID")
-rt.samples.read <- meta_data_read %>% filter(grepl("^Rt",SampleID))
-phywocont.rt.read <- prune_samples(rt.samples.read$SampleID,decontaminantsreadphy)
+rt.samples.read <- sample_names(decontaminantsreadphy)[which(grepl("^Rt",sample_names(decontaminantsreadphy)))]
+phywocont.rt.read <- prune_samples(rt.samples.read,decontaminantsreadphy)
 
 otu.rt.read <- data.frame(otu_table(phywocont.rt.read)) %>%
   select(which(colSums(.) > 0))  # remove empty taxa
@@ -168,11 +203,8 @@ colnames(otu.rt.read) <- str_replace(colnames(otu.rt.read), "X", "")
 rt.read <- colnames(otu.rt.read)
 physamples.read <- prune_taxa(rt.read, phywocont.rt.read)
 
-###10 taxa removed - rt samples only
-
-
 ##just extraction blanks - whats not there?
-blanksamples.read <- meta_data_read %>% filter(grepl("Blank", Sample.R_cat))
+blanksamples.read <- sample_names(decontaminantsreadphy)[which(grepl("^B", sample_names(decontaminantsreadphy)))]
 phywocont.blanks.read <- prune_samples(blanksamples.read$SampleID,decontaminantsreadphy)
 
 otu.blanks.read <- data.frame(otu_table(phywocont.blanks.read)) %>%
@@ -200,6 +232,7 @@ overlapphy.read <- prune_taxa(taxa_names(physwabs.read), phyblanks.read) #288 ta
 #facets for samples only, also in blanks, also in swabs, also in blanks + swabs = 4
 #need to tidy this up
 samplesonly <- data.frame(otu_table(phywocont.rt))
+plot_bo
 samplesonly.read <- data.frame(otu_table(phywocont.rt.read))
 inblanks <- data.frame(otu_table(phyblanks))
 inblanks.read <- data.frame(otu_table(phyblanks.read))
@@ -215,7 +248,6 @@ colnames(inswabs) <- str_replace(colnames(inswabs), "X", "")
 colnames(inswabs.read) <- str_replace(colnames(inswabs.read), "X", "")
 colnames(inswabsandblanks) <- str_replace(colnames(inswabsandblanks), "X", "")
 colnames(inswabsandblanks.read) <- str_replace(colnames(inswabsandblanks.read), "X", "")
-
 
 #trim in blansk to only those not in colanmes(Swabs and blanks)
 inblanks.read <- inblanks.read[,!(colnames(inblanks.read)) %in% colnames(inswabsandblanks.read)]
